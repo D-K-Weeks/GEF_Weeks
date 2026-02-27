8fdc62dd990dcf2ff051b72459f7089a2eda4cec
D-K-Weeks <161901831+D-K-Weeks@users.noreply.github.com>
2026-02-27 16:52:01 -0700

    InputHandler for Godot, Unity, UE
    InputHandlers in place for all engines

--- Godot/Scenes/Input Handler/inputhandler.gd
+++ Godot/Scenes/Input Handler/inputhandler.gd
@@ -6,3 +6,14 @@ func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("attack"):
 		attack_held = true
 		print("ATTACK PRESSED")
+
+	if Input.is_action_just_released("attack"):
+		attack_held = false
+		print ("ATTACK RELEASED")
+		
+	if attack_held:
+		print("ATTACK HELD")
+		
+	var move := Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	if move != Vector2.ZERO:
+		print("Move", move)
